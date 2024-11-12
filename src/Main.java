19d2285e3e3a49f2d3aabd04aa26d19d8bc08be7
Sachinkisna <sachinsoftware0001@gmail.com>
2024-11-12 10:24:18 +0530

    initial comment

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Identifiers");
+        //Here Are Rules For Java Identifiers
+        System.out.println("Only A-Z a-z 0-9 _ $ signs are allowed");
+        //Identifiers can't start with a number(0-9)
+        System.out.println("Java 'keywords' can't use and case sensitive");
+        /* Naming convention*/
+        System.out.println("Classes and interfaces use 'PascalCase' ");
+        System.out.println("Methods and variables use 'camelCase' ");
+        System.out.println("Constanta use 'ALL_UPPERCASE' ");
+
+    }
+}
